a4381c6e59f9bf6a0511cba0dca265c322eb1ec7
Hmiya6 <h.miya.work@gmail.com>
2021-05-06 17:14:10 +0900

    Small change

--- src/http.rs
+++ src/http.rs
@@ -2,6 +2,7 @@
 pub mod client;
 pub mod request;
 pub mod response;
+pub mod url;
 
 pub enum Method {
     GET,
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 
 pub mod http;
+pub mod utils;
+// pub mod renderer;
 
 
 fn main() {
