73a58201d7613494bd81d9015430b8717b94dfe3
Hmiya6 <h.miya.work@gmail.com>
2021-05-06 17:10:08 +0900

    Add tests and chage codes to pass the tests

--- src/http/client.rs
+++ src/http/client.rs
@@ -1,6 +1,8 @@
 
 use std::net;
-
+use std::io::prelude::*;
+use std::net::{TcpStream, ToSocketAddrs};
+use crate::http::{request::Request, response::Response, url::URL};
 
 pub struct Client {
 }
@@ -9,6 +11,46 @@ pub struct Client {
 impl Client {
     pub fn new() -> Self {
         Self {}
+    }
+
+    pub fn get(url: &str) -> Response {
+
+        let parsed_url = URL::parse(url);
+        let ip = format!("{}:{}", parsed_url.host(), parsed_url.port())
+            .to_socket_addrs()
+            .unwrap()
+            .nth(0)
+            .unwrap();
+
+        let mut req = Request::get(url);
+        let req = req.build();
+
+        let mut stream = TcpStream::connect(ip).unwrap();
+        stream.write_all(req.as_bytes()).unwrap();
+
+        let mut res = String::new();
+        stream.read_to_string(&mut res).unwrap();
+
+        stream.shutdown(net::Shutdown::Both).unwrap();
+
+        let res = Response::parse(&res);
+
+        res
+    }
+
+    // pub fn send(request: &Request) -> Response {
+    // 
+    // }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_get() {
+        Client::get("example.com");
     }
 }
 
--- src/http/request.rs
+++ src/http/request.rs
@@ -1,42 +1,69 @@
 use std::collections::HashMap;
 use crate::http::Method;
+use crate::http::url::URL;
+
+// Example HTTP Request
+// ```
+// GET / HTTP/1.1
+// Host: example.com
+// User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+// Accept-Language: en-US,en;q=0.5
+// Accept-Encoding: gzip, deflate
+// Connection: keep-alive
+// Upgrade-Insecure-Requests: 1
+// If-Modified-Since: Thu, 17 Oct 2019 07:18:26 GMT
+// If-None-Match: "3147526947"
+// Cache-Control: max-age=0
+// ```
 
 pub struct Request {
     line: RequestLine,
     head: Head,
-    body: Option<String>,
+    body: String,
 }
 
 impl Request {
 
     pub fn get(url: &str) -> Self {
-        
-    }
-
-    pub fn post(url: &str, body: &str) -> Self {
-
-    }
-
-    pub fn new(url: &str, method: Method, head: Head, body: Option<&str>) -> Self {
         let url = URL::parse(url);
+        Self::new(
+            &url,
+            Method::GET,
+            Head::new(&url.host()),
+            None,
+        )
+    }
+
+    pub fn read_host(&self) -> String {
+        let head = &self.head;
+        let host = match head.0.get("Host") {
+            Some(s) => s.clone(),
+            None => panic!("No valid Host"),
+        };
+        host
+    }
+
+    // pub fn post(url: &str, body: &str) -> Self {
+    //
+    // }
+
+    pub fn new(url: &URL, method: Method, head: Head, body: Option<String>) -> Self {
         Self {
-            line: RequestLine::new(method, )
+            line: RequestLine::new(method, &url.path(), &url.scheme()),
+            head,
+            body: match body {
+                Some(s) => s,
+                None => "".to_string(),
+            },
         }
 
     }
 
-    fn build(&self) -> String {
-        
+    pub fn build(&mut self) -> String {
+        format!("{}{}\r\n{}", self.line.build(), self.head.build(), self.body)
     }
-
-
-    fn build_head(&self) -> String {
-    }
-
-    fn build_body(&self) -> String {
-
-    }
-    
+ 
 }
 
 struct RequestLine {
@@ -61,7 +88,7 @@ impl RequestLine {
             Method::POST => "POST",
         };
 
-        format!("{} {} {}\n", method, self.path, self.protocol)
+        format!("{} {} {}\r\n", method, self.path, "HTTP/1.0")
     }
 }
 
@@ -69,8 +96,10 @@ impl RequestLine {
 pub struct Head(HashMap<String, String>);
 
 impl Head {
-    pub fn new() -> Self {
-        Self(HashMap::new())
+    pub fn new(host: &str) -> Self {
+        let mut head = Self(HashMap::new());
+        head.add("Host", host);
+        head
     }
 
     pub fn add(&mut self, key: &str, val: &str) {
@@ -80,23 +109,44 @@ impl Head {
     pub fn remove(&mut self, key: &str) {
         self.0.remove(key);
     }
-}
 
-
-// URL = (scheme "://")? host (":" port)? (path)?
-pub struct URL {
-    scheme: String,
-    host: String,
-    port: String,
-    path: String,
-}
-
-impl URL {
-    parse(url: &str) -> Self {
-        
+    pub fn build(&mut self) -> String {
+        let mut result = format!("Host: {}\r\n", self.0["Host"]);
+        self.remove("Host");
+        for (k, v) in self.0.drain() {
+            result = format!("{}{}:{}\r\n", result, k, v);
+        }
+        result
     }
 }
 
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let url = "example.com";
+
+        let req = Request::get(url);
+
+        assert_eq!(req.line.path, "/");
+        assert_eq!(req.line.protocol, "http");
+        assert_eq!(req.head.0.get("Host"), Some(&"example.com".to_string()));
+        assert_eq!(req.body, String::new());
+    }
+    
+    #[test]
+    fn test_build() {
+        let mut req = Request::get("example.com");
+        let raw_req = req.build();
+
+        println!("{}", raw_req);
+    }
+
+}
 
 
 
