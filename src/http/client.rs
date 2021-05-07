f70ed5e0d32bb7e431406b5c68069de8ac0b2570
Hmiya6 <h.miya.work@gmail.com>
2021-05-07 11:12:01 +0900

    Add comment

--- src/http/client.rs
+++ src/http/client.rs
@@ -8,7 +8,7 @@ use std::error::Error;
 // ERROR HANDLING ---------------------
 #[derive(Debug)]
 pub enum ClientError {
-
+    
 }
 
 impl fmt::Display for ClientError {
@@ -33,25 +33,30 @@ impl Client {
     }
 
     pub fn get(url: &str) -> Result<Response, Box<dyn Error>> {
-
+        
+        // get IP address using OS's DNS resolver
         let parsed_url = URL::parse(url)?;
-        let ip = format!("{}:{}", parsed_url.host(), parsed_url.port())
-            .to_socket_addrs()
-            .unwrap()
-            .nth(0)
+        let ip = format!("{}:{}", parsed_url.host(), parsed_url.port()) // create valid URL to find IP address
+            .to_socket_addrs()? // get IP addresses from resolver
+            .nth(0) // get IPv4 address
             .unwrap();
-
+        
+        // create GET request from URL
         let mut req = Request::get(url)?;
         let req = req.build()?;
-
+        
+        // send request using OS's TCP socket
         let mut stream = TcpStream::connect(ip)?;
         stream.write_all(req.as_bytes())?;
-
+        
+        // read response
         let mut res = String::new();
         stream.read_to_string(&mut res)?;
-
+        
+        // shutdown TCP connection
         stream.shutdown(net::Shutdown::Both)?;
-
+        
+        // parse response
         let res = Response::parse(&res)?;
 
         Ok(res)
